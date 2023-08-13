2b5023f5575d04320363d9dfb19ffe538648d0b6
bshivaraj <shivarajbiradar66@gmail.com>
2023-08-13 20:13:07 +0530

    adding controller

--- counter-app/src/main/java/com/counter/app/CounterAppApplication.java
+++ counter-app/src/main/java/com/counter/app/CounterAppApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.counter.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ counter-app/src/main/java/com/counter/controller/CounterController.java
@@ -0,0 +1,5 @@
+package com.counter.controller;
+
+public class CounterController {
+
+}
